70530679c4c6aa2b175ac6c02a92d33394708865
sv99 <sv99@inbox.ru>
2016-09-20 22:26:15 +0300

    hunterized project and add pattern.md

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,1 @@
+hunter_config(spdlog VERSION 0.10.0)
--- tests/version_check.cpp
+++ tests/version_check.cpp
@@ -6,5 +6,5 @@
 #include "config.h"
 
 TEST(version_check, test_eq) {
-  EXPECT_STREQ (get_version ().c_str (), "sms_gate version 0.1.0");
+  EXPECT_STREQ (get_version ().c_str (), "sms_gate version 0.2.0");
 }
